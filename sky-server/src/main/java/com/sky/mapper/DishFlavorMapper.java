5cba9ad0ee81e414ff435257f9ea5897d6434d66
Peisong Ma <pipchief1@gamil.com>
2025-04-10 14:14:52 +1000

    新增菜品业务代码开发

---
+++ sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
@@ -0,0 +1,15 @@
+package com.sky.mapper;
+
+import com.sky.entity.DishFlavor;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface DishFlavorMapper {
+    /**
+     * 批量插入口碑数据
+     */
+
+    void insertBatch(List<DishFlavor> flavors);
+}
