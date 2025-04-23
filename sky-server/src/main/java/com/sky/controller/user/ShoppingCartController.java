d2bb50501730082b405e7d195205693a892ce329
Peisong Ma <pipchief1@gmail.com>
2025-04-23 17:02:20 +1000

    清空购物车

--- sky-server/src/main/java/com/sky/controller/user/ShoppingCartController.java
+++ sky-server/src/main/java/com/sky/controller/user/ShoppingCartController.java
@@ -36,9 +36,16 @@ public class ShoppingCartController {
      *查看购物车
      */
     @GetMapping("/list")
-    @ApiOperation("查看公务车")
+    @ApiOperation("查看购物车")
     public Result<List<ShoppingCart >> list(){
         List<ShoppingCart> list = shoppingCartService.showShoppingCart();
         return Result.success(list);
+
+    }
+    @DeleteMapping("/clean")
+    @ApiOperation("清空购物车")
+    public Result clean(){
+        shoppingCartService.cleanShoppingCart();
+        return Result.success();
     }
 }
