91ed222fdbfa1374a7faf530953afd48a98ce8d1
Peisong Ma <pipchief1@gmail.com>
2025-04-23 13:33:03 +1000

    缓存菜品开发

--- sky-server/src/main/java/com/sky/controller/user/DishController.java
+++ sky-server/src/main/java/com/sky/controller/user/DishController.java
@@ -8,7 +8,9 @@ import com.sky.vo.DishVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.xmlbeans.xml.stream.BufferedXMLInputStream;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +24,8 @@ public class DishController {
     @Autowired
     private DishService dishService;
 
+    @Autowired
+    private RedisTemplate redisTemplate;
     /**
      * 根据分类id查询菜品
      *
@@ -31,12 +35,23 @@ public class DishController {
     @GetMapping("/list")
     @ApiOperation("根据分类id查询菜品")
     public Result<List<DishVO>> list(Long categoryId) {
+        //构造redis的key规则:dish_分类id
+        String key = "dish_"+categoryId;
+        List<DishVO> list  =(List<DishVO>) redisTemplate.opsForValue().get(key);
+
+
+        //查询redis中是否存在缓存数据
+        if(list != null && list.size() > 0) {
+            //如果存在直接返回，不需要查询数据库
+            return Result.success(list);
+        }
+        //如果不存在，查询数据库，并把查询的数据放到redis中
         Dish dish = new Dish();
         dish.setCategoryId(categoryId);
         dish.setStatus(StatusConstant.ENABLE);//查询起售中的菜品
 
-        List<DishVO> list = dishService.listWithFlavor(dish);
-
+        list = dishService.listWithFlavor(dish);
+        redisTemplate.opsForValue().set(key,list);
         return Result.success(list);
     }
 
