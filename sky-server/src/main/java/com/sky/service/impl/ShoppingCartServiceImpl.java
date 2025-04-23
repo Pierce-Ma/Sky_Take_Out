074ab7953ed361892be7a594b154bc89a6970f50
Peisong Ma <pipchief1@gmail.com>
2025-04-23 16:20:25 +1000

    添加购物车代码

---
+++ sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
@@ -0,0 +1,77 @@
+package com.sky.service.impl;
+
+import com.sky.context.BaseContext;
+import com.sky.dto.ShoppingCartDTO;
+import com.sky.entity.Dish;
+import com.sky.entity.Setmeal;
+import com.sky.entity.ShoppingCart;
+import com.sky.mapper.DishMapper;
+import com.sky.mapper.SetmealMapper;
+import com.sky.mapper.ShoppingCartMapper;
+import com.sky.service.ShoppingCartService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@Slf4j
+public class ShoppingCartServiceImpl implements ShoppingCartService {
+
+    @Autowired
+    private ShoppingCartMapper shoppingCartMapper;
+
+    @Autowired
+    private DishMapper dishMapper;
+
+    @Autowired
+    private SetmealMapper setmealMapper;
+
+    public void addShoppingCart(ShoppingCartDTO shoppingCartDTO) {
+        //添加购物车时候，先判断是否已经存在中，
+        ShoppingCart shoppingCart = new ShoppingCart();
+        //通过属性拷贝来拷过来,但是注意，DTO对象里并没有UserID，但我们其实在解析器
+        //那里已经把id放到线程里了
+        BeanUtils.copyProperties(shoppingCartDTO, shoppingCart);
+        Long userId = BaseContext.getCurrentId();
+        shoppingCart.setUserId(userId);
+
+         List<ShoppingCart> list = shoppingCartMapper.list(shoppingCart);
+        // 如果存在了， 只需要进行update操作进行更新
+        if(list != null && list.size() > 0){
+            ShoppingCart cart = list.get(0);
+            cart.setNumber(cart.getNumber()+1);//update shopping cart set number = ? where id = ?
+            shoppingCartMapper.updateNumberByid(cart);
+        }else{
+            //判断本次添加到购物车的是菜品还是套餐
+            Long dishId = shoppingCartDTO.getDishId();
+
+            if(dishId != null ){
+                //本次是菜品
+                Dish dish = dishMapper.getById(dishId);
+                shoppingCart.setName(dish.getName());
+                shoppingCart.setImage(dish.getImage());
+                shoppingCart.setAmount(dish.getPrice());
+
+
+            }
+            else{
+                //本次是套餐
+                Long setmealId = shoppingCartDTO.getSetmealId();
+                Setmeal setmeal= setmealMapper.getById(setmealId);
+                shoppingCart.setName(setmeal.getName());
+                shoppingCart.setImage(setmeal.getImage());
+                shoppingCart.setAmount(setmeal.getPrice());
+
+            }
+            shoppingCart.setNumber(1);
+            shoppingCart.setCreateTime(LocalDateTime.now());
+            shoppingCartMapper.insert(shoppingCart);
+        }
+        //如果不存在的时候，需要insert
+    }
+}
